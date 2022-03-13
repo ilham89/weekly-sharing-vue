fd83d46c17849e593b52a253be5b7048cee6de56
Ilham Kukuh Prakosa <ilham.kodingworks@gmail.com>
2022-03-13 17:02:53 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <h5>{{ this.count }}</h5>
+    <button @click="addCount">tambah</button>
+
+    <h4>{{ valueInput }}</h4>
+    <input type="input" v-model="valueInput" />
+    <h4>{{ this.status }}</h4>
+    <div v-if="status">status bernilai true</div>
+    <div v-else>status bernilai false</div>
+    <button @click="changeStatus">ubah status</button>
+
+    <div v-for="(data, index) in datas" :key="index">{{ data }}</div> -->
+
+    <h4>Todo List</h4>
+    <h6>{{ this.angkaAcak() }}</h6>
+    <h6>{{ checkValueInput }}</h6>
+    <input v-model="valueInput" v-on:keyup.enter="addTodos" />
+    <button @click="addTodos">Tambah</button>
+    <Activities :todos="todos" @deleteTodo="deleteTodo" />
+
+    <div>Total todo: {{ this.totalTodo }}</div>
+    <div>Belum selesai: {{ this.undoneTodo }}</div>
+    <div>Sudah selesai: {{ this.doneTodo }}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Activities from "./components/Activities.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        Activities,
+    },
+    data() {
+        return {
+            count: 0,
+            valueInput: "",
+            status: true,
+            datas: ["ilham", "kukuh", "prakosa", "tees"],
+            todos: [],
+        };
+    },
+    computed: {
+        totalTodo() {
+            return this.todos.length;
+        },
+        doneTodo() {
+            return this.todos.filter((todo) => todo.isDone).length;
+        },
+        undoneTodo() {
+            return this.todos.filter((todo) => !todo.isDone).length;
+        },
+        checkValueInput() {
+            return this.angka1 + this.angka2;
+        },
+    },
+    methods: {
+        addCount() {
+            this.count++;
+        },
+        changeStatus() {
+            this.status = !this.status;
+        },
+        addTodos() {
+            this.todos.push({
+                activity: this.valueInput,
+                isDone: false,
+            });
+            this.valueInput = "";
+        },
+        deleteTodo(todoIndex) {
+            this.todos.splice(todoIndex, 1);
+        },
+        angkaAcak() {
+            return Math.random();
+        },
+    },
+    watch: {
+        valueInput(newVal) {
+            if (newVal) {
+                this.angkaAcak();
+            }
+        },
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    /* font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px; */
 }
 </style>
---
+++ src/components/Activities.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="hello">
+        <ol>
+            <li v-for="(todo, index) in todos" :key="index">
+                <div class="todo">
+                    <div v-if="!todo.isDone">
+                        {{ todo.activity }}
+                    </div>
+                    <s v-else>{{ todo.activity }}</s>
+
+                    <input type="checkbox" v-model="todo.isDone" />
+                    <button @click="deleteTodo(index)">Hapus</button>
+                </div>
+            </li>
+        </ol>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Activities",
+    props: {
+        todos: {
+            type: Array,
+            default: () => [],
+        },
+    },
+    methods: {
+        deleteTodo(index) {
+            this.$emit("deleteTodo", index);
+        },
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.todo {
+    display: flex;
+}
+</style>
